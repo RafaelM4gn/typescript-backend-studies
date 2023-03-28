a34bad0379c47e064d2d5c3428848a3347c1494b
RafaelM4gn <rfl.magno02@gmail.com>
2023-03-27 22:58:24 -0300

    implement CRUD and add MySQL2

--- index.ts
+++ index.ts
@@ -1,11 +1,51 @@
+// Imports
 import express from 'express'
+import mysql from 'mysql2/promise'
 
+// Inicialize express app
 const app = express()
 
-app.get('/', async (req, res) => {
-    res.send("Hello World")
+// Middleware to parse json data
+app.use(express.json())
+
+// Create connection pool to mysql database
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'banco',
+  connectionLimit: 10
 })
 
+// Read and return all table
+app.get('/', async (req, res) => {
+  const [notes] = await pool.query('SELECT * FROM notas');
+  res.json(notes)
+})
+
+// Update a note by id
+app.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const { title, content } = req.body
+  await pool.query('UPDATE notas SET title = ?, content = ? WHERE id = ?', [title, content, id])
+  res.json({ message: 'Note updated' })
+})
+
+// Create a new note
+app.post('/', async (req, res) => {
+  const { title, content } = req.body
+  await pool.query('INSERT INTO notas (title, content) VALUES (?, ?)', [title, content])
+  res.json({ message: 'Note created' })
+})
+
+// Remove a note by id
+app.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  await pool.query('DELETE FROM notas WHERE id = ?', [id])
+  res.json({ message: 'Note deleted' })
+})
+
+// listen on port 3000
 app.listen(3000, () => {
-    console.log('Server is running')
+  console.log('Server is running')
 })
